bb1f2d8e6d9cead8c83fc21a989feed19c715dbe
Tetiana Pavlenko <72665178+tpavlenko@users.noreply.github.com>
2021-04-13 10:28:37 +0300

    Add files via upload

--- script5.js
+++ script5.js
@@ -2,27 +2,26 @@
 // 	assignObjects(obj1, obj2, ....., objn);
 
 
-function assignObjects() {
-	var assigned = {};
-	var flag = typeof arguments[arguments.length - 1] == 'boolean' ? arguments[arguments.length - 1] : false
+function assignObjects () {
+		var obj = {};
+		var flag = typeof arguments[arguments.length - 1] == 'boolean' ? arguments[arguments.length - 1] : false
 
-	for (var i = 0; i < arguments.length; i++) {
-		var args = arguments[i];
-		for (var key in args) {
-
-			if (flag && assigned.hasOwnProperty(key)) {
-				continue
-			} else {
-				assigned[key] = args[key];
+		for (var i = 0; i < arguments.length; i++) {
+			var args = arguments[i];
+			for (var key in args) {
+				if (flag && obj.hasOwnProperty(key)) {
+					continue
+				} else {
+					obj[key] = args[key];
+				}
 			}
 		}
+		return obj;
 	}
-	return assigned;
-}
 
 var obj1 = { a: 1, b: 2, };
-var obj2 = { c: 3, d: 4, };
-var obj3 = { d: 5, b: 0, };
+var obj2 = { a: 30, d: 4, };
+var obj3 = { c: 5, d: 100, };
 
 var result = assignObjects(obj1, obj2, obj3, true);
 
